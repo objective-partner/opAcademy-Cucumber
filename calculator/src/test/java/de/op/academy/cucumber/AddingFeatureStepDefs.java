2f8624a93efaf602a8862449a4e28b1e863dbaa0
Thorsten Gawantka <t.gawantka@googlemail.com>
2012-01-31 14:44:43 +0100

    Added pending step-implementations.

--- calculator/src/test/java/de/op/academy/cucumber/AddingFeatureStepDefs.java
+++ calculator/src/test/java/de/op/academy/cucumber/AddingFeatureStepDefs.java
@@ -1,6 +1,29 @@
 package de.op.academy.cucumber;
 
+import cucumber.annotation.Pending;
+import cucumber.annotation.en.Given;
+import cucumber.annotation.en.Then;
+import cucumber.annotation.en.When;
+
 public class AddingFeatureStepDefs {
 
 	private Calculator calculator;
+	
+	@Given("^a calculator I just turned on$")
+	@Pending
+	public void aCalculatorIJustTurnedOn() {
+	    // Express the Regexp above with the code you wish you had
+	}
+
+	@When("^I add (\\d+) and (\\d+)$")
+	@Pending
+	public void iAddAnd(int arg1, int arg2) {
+	    // Express the Regexp above with the code you wish you had
+	}
+
+	@Then("^the result is (\\d+)$")
+	@Pending
+	public void theResultIs(int arg1) {
+	    // Express the Regexp above with the code you wish you had
+	}
 }
